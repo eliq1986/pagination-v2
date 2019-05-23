8e74c5921538228ca301c3620bacdcdb0658bc66
Elijah Quesada <this_boy10@yahoo.com>
2019-05-23 12:21:15 -0700

    Added obtrusive JS with function lines 19-30.

--- js/script.js
+++ js/script.js
@@ -1,25 +1,55 @@
 // Global Variables
-const studentListItems = document.querySelectorAll(".student-item");
+const studentListItems = document.querySelectorAll("li.student-item");
 const maxStudentsPerPage = 10;
 
+//function invoked upon page load
+function onPageLoad() {
+  pagination(studentListItems,1);
+  showPage(studentListItems, 1);
+  appendsObtrusiveJS();
+}
+
+//returns start index; takes two parameters.
 const getStartIndex = (page, maxPerPage) => (page  * maxStudentsPerPage) - maxStudentsPerPage;
 
+
+//returns end index; takes two parameters
 const getEndIndex = (page, maxPerPage) => page * maxStudentsPerPage;
 
+function appendsObtrusiveJS() {
+  const div = document.createElement("div");
+  div.className = "student-search";
+
+  const input = document.createElement("input");
+  input.placeholder = "Search for students...";
+  const button = document.createElement("button");
+  button.textContent = "Search";
+  div.appendChild(input);
+  div.appendChild(button);
+
+  document.querySelector("div.page-header").appendChild(div);
+}
+
+//takes 3 arguments; loops over student list and displays block or none
+function displayStudentsItem(listOfStudents,startIndex, endIndex) {
+  for(let i = 0; i < listOfStudents.length; i++) {
+
+     if([i] >= startIndex && [i] < endIndex) {
+       listOfStudents[i].style.display = "block";
+     }else {
+       listOfStudents[i].style.display = "none";
+     }
+  }
+
+}
+
+
+//function calls three functions within that show appropriate list.
 function showPage(list, page){
 
-const startIndex = getStartIndex(page, maxStudentsPerPage);
-const endIndex = getEndIndex(page, maxStudentsPerPage);
-
-
-for(let i = 0; i < studentListItems.length; i++) {
-
-   if([i] >= startIndex && [i] < endIndex) {
-     studentListItems[i].style.display = "block";
-   }else {
-     studentListItems[i].style.display = "none";
-   }
-}
+  const startIndex = getStartIndex(page, maxStudentsPerPage);
+  const endIndex = getEndIndex(page, maxStudentsPerPage);
+  displayStudentsItem(list, startIndex, endIndex);
 
 }
 
@@ -41,6 +71,7 @@ function setFirstLinkActiveClass(page) {
 document.querySelectorAll("div.pagination a")[page - 1].className = "active";
 }
 
+
 function pagination(list, page) {
 const containerDiv = document.querySelector("div.page");
 const jsContainerAttribute = document.createAttribute("js-container");
@@ -58,9 +89,7 @@ appendLinks(list, page);
 setFirstLinkActiveClass(page);
 }
 
-//Invoked function calls set up page load
-pagination(studentListItems,1);
-showPage(studentListItems, 1);
+
 
 
 function setActiveClass(event) {
@@ -72,6 +101,9 @@ function setActiveClass(event) {
   paginationLinks.forEach(link => link.className = "");
   paginationLinks[pageNumber - 1].className = "active";
 }
+
+//******Invoked function calls set up page load******//
+onPageLoad();
 
 //Click event is captuered by parent UL element
 document.querySelector("div.pagination ul").addEventListener("click", event => {
